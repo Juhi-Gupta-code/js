6faa4b5e9b1ec0d9ee71e31061eba2734059f3a9
Juhi-Gupta-code <juhi18.dav@gmail.com>
2025-12-27 15:01:35 +0000

    Update comparison operators examples

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,24 @@
+ console.log(2>1);
+ console.log(2>=1);
+ console.log(2<1);
+ console.log(2==1);
+ console.log(2!=1);
+
+
+ console.log("2">1);
+ console.log("02">1);
+
+
+ console.log(null>0)// null is converted to 0
+ console.log(null==0)
+ console.log(null>=0)
+
+ console.log(undefined==0);
+ //using undefined with any operation gives false
+ console.log(undefined<0);
+ console.log(undefined>0);
+
+ //=== strict check // check data type also
+ console.log("2"==2)// does not check datatype
+ console.log("2"===2)//checks datatype and tells that they are not same 
+
